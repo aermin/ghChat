6920c9ccdb4e356feba3edcd0c5ec534857613c4
aermin <aermin@foxmail.com>
2018-06-24 21:27:43 +0800

    header component complete  50%

---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {setTabShow} from "../../redux/actions/whichShow";
+import './style.scss';
+
+const SearchBox = () =>{
+    return (
+        <div className="search-box">
+            <svg className="icon" aria-hidden="true"> <use  xlinkHref="#icon-search1"></use></svg>
+            <input type="text"  placeholder="搜索用户/群" />
+        </div>  
+    )
+}
+
+export default class Header extends Component {
+	constructor(){
+		super();
+         	this.state = {
+                userInfo:{}
+             }
+        }
+        componentWillMount(){
+            this.setState({
+                userInfo:JSON.parse(localStorage.getItem("userInfo"))
+            }) 
+        }
+        render() {
+            console.log("userInfo", this.state.userInfo);
+            return (
+                <div className="header-wrapper">
+                    <img src={this.state.userInfo.avator} alt=""/>
+                    <SearchBox />
+                </div>
+            )
+       }
+}
+
+// export default connect(state => ({
+//     tabShow: state.whichShow.tabShow
+//   }), {
+//     setTabShow
+//   })(Header);
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import HomePageList from '../HomePageList/index';
+import HomePageList from '../HomePageList';
 import Tabs from '../../components/Tabs';
+import Header from '../../components/Header';
 import './style.scss';
 
 
@@ -15,9 +16,9 @@ export default class Layout extends Component {
             return (
                 <div className = 'layout-wrapper'>
                        <div className = 'layout-left'>
+                             <Header />
                              <Tabs />
                             <HomePageList />
-                        
                        </div>
                        <div className= 'layout-right'>
 
