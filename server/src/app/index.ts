0a28f1fd23dcd01dc220e089ce8777e42689de44
AlanZou <e936106@gmail.com>
2019-04-14 02:26:21 +0800

    remove not necessary middleware

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,26 +1,19 @@
 import { environment } from '@env';
 import * as bodyParser from 'koa-bodyparser';
 import * as compress from 'koa-compress';
-import * as statics from 'koa-static';
 import * as cors from 'koa2-cors';
-import { join } from 'path';
 
 import { ServicesContext } from './context';
 import { appRoutes } from './routes';
 import { Server } from './server';
 import { ChatService, GroupChatService, GroupService, UserService } from './services';
 
-// 配置静态资源
-const staticPath = '../build';
-
 Server
   .init((app) => {
     app.use(compress())
       .use(cors())
       .use(bodyParser())
-      .use(statics(
-        join(__dirname, staticPath)
-      ))
+      // .use(koa2FallbackApiMiddleware())
       .use(appRoutes.routes())
       .use(appRoutes.allowedMethods())
   })
