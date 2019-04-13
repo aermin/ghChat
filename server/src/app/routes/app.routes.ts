f90f788fe984e5e91291ac05c5ac8caf96bc604f
AlanZou <e936106@gmail.com>
2019-04-14 01:07:11 +0800

    add default server check route

--- server/src/app/routes/app.routes.ts
+++ server/src/app/routes/app.routes.ts
@@ -2,10 +2,20 @@ import * as router from 'koa-router';
 
 import { githubOAuthController, loginController, registerController } from '../controllers';
 import { environment } from '@env';
+import { koa2FallbackApiMiddleware } from '../middlewares/koa2FallbackApi.middleware';
 
 
 export const appRoutes = new router()
   .prefix(`/${environment.baseApi}`)
+  .get('/', async (ctx, next) => {
+    await next();
+    ctx.body = {
+      message: 'server alive',
+      time: new Date()
+    };
+    ctx.status = 200;
+  })
+  .use(koa2FallbackApiMiddleware())
   .post('/register', registerController) // 注册
   .post('/login', loginController) // 登录
   .post('/github_oauth', githubOAuthController);
