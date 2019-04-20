8ac7968a93e65a190c7b135f2e83e49491122a0e
aermin <aermin@qq.com>
2019-04-20 19:54:39 +0800

    chore: change time format (#21)
    * chore: change time format
    
    * add newline

--- src/utils/transformTime.js
+++ src/utils/transformTime.js
@@ -12,21 +12,35 @@ function addZero(data) {
   return data;
 }
 
+function monthHandle(month) {
+  return `${month + 1 < 10 ? `0${month + 1}` : month + 1}`;
+}
+
 export const toNormalTime = (timestamp) => {
-  const date = new Date(timestamp * 1000);
-  const dayOfArg = addZero(date.getDate());
-  const hourOfArg = addZero(date.getHours());
-  const minuteOfArg = addZero(date.getMinutes());
-  const today = new Date().getDate();
-  const thisYear = new Date().getFullYear();
-  // 显示当天几点
-  if (date.getDate() === today) {
-    return `${hourOfArg}: ${minuteOfArg}`;
+  const dateOfArg = new Date(timestamp * 1000);
+  const [yearOfArg, monthOfArg, dayOfArg, hourOfArg, minuteOfArg] = [
+    dateOfArg.getFullYear(), dateOfArg.getMonth(), dateOfArg.getDate(),
+    dateOfArg.getHours(), dateOfArg.getMinutes()
+  ];
+  const date = new Date();
+  const [today, yesterday, thisMonth, thisYear] = [
+    date.getDate(), new Date(new Date().setDate(new Date().getDate() - 1)).getDate(),
+    date.getMonth(), date.getFullYear()
+  ];
+
+  const isToday = thisYear === yearOfArg && thisMonth === monthOfArg && today === dayOfArg;
+  if (isToday) {
+    return `${addZero(hourOfArg)}: ${addZero(minuteOfArg)}`;
   }
-  const yearOfArg = date.getFullYear();
-  const monthOfArg = `${date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}`;
+
+  const isYesterday = thisYear === yearOfArg && thisMonth === monthOfArg && yesterday === dayOfArg;
+  if (isYesterday) {
+    return `昨天 ${addZero(hourOfArg)}: ${addZero(minuteOfArg)}`;
+  }
+
   if (yearOfArg === thisYear) {
-    return `${monthOfArg}/${dayOfArg}`;
+    return `${monthHandle(monthOfArg)}/${addZero(dayOfArg)}`;
   }
-  return `${yearOfArg}/${monthOfArg}/${dayOfArg}`;
+
+  return `${yearOfArg}/${monthHandle(monthOfArg)}/${addZero(dayOfArg)}`;
 };
