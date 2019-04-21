805271fdf904b008ee056087a54bbde299744bc2
aermin <aermin@foxmail.com>
2019-04-21 13:02:15 +0800

    feat: add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Switch
+} from 'react-router-dom';
 import loadable from '@loadable/component';
-import Tabs from '../components/Tabs';
+
+const FUNCTION_ROUTERS = ['/', '/robot_chat', '/group_chat/:to_group_id', '/private_chat/:user_id', '/setting'];
+const AUTH_ROUTERS = ['/login', 'register'];
 
 function MainView(props) {
   const { pathname } = props.location;
-  if (pathname !== '/login' && pathname !== '/register' && !localStorage.getItem('userInfo')) {
+  if (AUTH_ROUTERS.indexOf(pathname) < 0 && !localStorage.getItem('userInfo')) {
     sessionStorage.setItem('originalLink', window.location.href);
     props.history.push('/login');
   }
+
   let MainViewClassName;
   if (pathname === '/' || pathname === '/setting') {
     MainViewClassName = 'layout-left';
   } else {
     MainViewClassName = 'layout-left-mobile';
   }
+
   return (
     <div className={MainViewClassName}>
-      <Tabs />
+      <Route component={loadable(() => import('../components/Tabs'))} />
       <Route path={['/', '/robot_chat', '/group_chat/:to_group_id', '/private_chat/:user_id']} exact component={loadable(() => import('../containers/HomePageList'))} />
       <Route path="/setting" exact component={loadable(() => import('../containers/SettingPage'))} />
     </div>
@@ -27,11 +33,13 @@ function MainView(props) {
 function RightView(props) {
   const { pathname } = props.location;
   let RightViewClassName;
+
   if (pathname === '/' || pathname === '/setting') {
     RightViewClassName = 'layout-right-mobile';
   } else {
     RightViewClassName = 'layout-right';
   }
+
   return (
     <div className={RightViewClassName}>
       <Route path="/robot_chat" component={loadable(() => import('../containers/RobotPage'))} />
@@ -46,10 +54,15 @@ export default function getRouter() {
   return (
     <Router>
       <div className="layout-wrapper">
-        <Route path="/register" component={loadable(() => import('../containers/RegisterPage'))} />
-        <Route path="/login" component={loadable(() => import('../containers/LogInPage'))} />
-        <Route path={['/', '/robot_chat', '/group_chat/:to_group_id', '/private_chat/:user_id', '/setting']} exact component={MainView} />
-        <Route path={['/', '/robot_chat', '/group_chat/:to_group_id', '/private_chat/:user_id', '/setting']} exact component={RightView} />
+        <Switch>
+          <Route path="/register" exact component={loadable(() => import('../containers/RegisterPage'))} />
+          <Route path="/login" exact component={loadable(() => import('../containers/LogInPage'))} />
+          <Route exact path={FUNCTION_ROUTERS}>
+            <Route path={FUNCTION_ROUTERS} exact component={MainView} />
+            <Route path={FUNCTION_ROUTERS} exact component={RightView} />
+          </Route>
+          <Route component={loadable(() => import('../components/NotFound'))} />
+        </Switch>
       </div>
     </Router>
   );
