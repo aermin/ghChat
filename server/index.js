a18109060267110e267adceb25aa51171b50a46b
aermin <aermin@foxmail.com>
2019-05-08 18:46:51 +0800

    refactor: refactor server router to more generic

--- server/index.js
+++ server/index.js
@@ -3,12 +3,11 @@ const bodyParser = require('koa-bodyparser');
 const cors = require('koa2-cors');
 const compress = require('koa-compress');
 const http = require('http');
-const statics = require('koa-static'); // 静态资源服务插件
-const path = require('path'); // 路径管理
+const statics = require('koa-static');
+const path = require('path');
 const socketHandle = require('./socket');
 const router = require('./routes/index');
 const { query } = require('./utils/db');
-const koa2FallbackApiMiddleware = require('./middlewares/koa2FallbackApiMiddleware');
 
 const app = new Koa();
 
@@ -24,8 +23,6 @@ app.use(bodyParser());
 console.log('server node env', process.env.NODE_ENV);
 
 app.use(router.routes()).use(router.allowedMethods());
-
-app.use(koa2FallbackApiMiddleware());
 
 global.query = query;
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,20 @@
+const path = require('path');
 const router = require('koa-router')();
 const baseApi = require('../config').baseApi;
 const register = require('../controllers/register');
 const login = require('../controllers/login');
 const githubOAuth = require('../controllers/githubOAuth');
 
-router.prefix(`/${baseApi}`);
-router.post('/register', register) // 注册
+const apiRoutes = router
+  .post('/register', register) // 注册
   .post('/login', login) // 登录
   .post('/github_oauth', githubOAuth);
+
+
+router.use(
+  path.join('/', baseApi),
+  apiRoutes.routes(),
+  apiRoutes.allowedMethods()
+);
 
 module.exports = router;
