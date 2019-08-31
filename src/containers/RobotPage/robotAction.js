19e52eb66c4eba9f4660272718bcf2b7affca473
aermin <aermin@foxmail.com>
2019-08-31 17:44:29 +0800

    fix: robot message prompt

--- src/containers/RobotPage/robotAction.js
+++ src/containers/RobotPage/robotAction.js
@@ -10,29 +10,19 @@ export const insertMsgAction = data => ({
 });
 
 export const getRobotMsgAction = async (data) => {
-  let finalData = {};
-  const response = await request.socketEmitAndGetResponse('robotChat', data, (error) => {
-    notification('消息发送失败', 'error', 2);
-  });
+  const response = await request.socketEmitAndGetResponse(
+    'robotChat',
+    data,
+    (error) => {
+      notification('消息发送失败', 'error', 2);
+    }
+  );
   const { text, code, url } = response;
-  if (code === 100000) {
-    finalData = {
-      message: text,
-      user: '机器人小R'
-    };
-  } else if (code === 200000) {
-    finalData = {
-      message: text + url,
-      user: '机器人小R'
-    };
-  } else {
-    finalData = {
-      message: '暂不支持此类对话',
-      user: '机器人小R'
-    };
-  }
   return {
     type: INSERT_MSG,
-    data: finalData
+    data: {
+      message: code === 200000 ? text + url : text,
+      user: '机器人小R'
+    }
   };
 };
